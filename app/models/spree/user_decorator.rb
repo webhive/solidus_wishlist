ba7f7c177fbba094d684ef7508f32d7f13851112
michele franzin <michele.franzin@seesaw.it>
2019-11-26 17:03:07 +0100

    Solidus deprecation warnings fixed all locales are now nester in 'wishlist' namespace

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -4,7 +4,7 @@ Spree.user_class.class_eval do
   def wishlist
     default_wishlist = wishlists.where(is_default: true).first
     default_wishlist ||= wishlists.first
-    default_wishlist ||= wishlists.create(name: Spree.t(:default_wishlist_name), is_default: true)
+    default_wishlist ||= wishlists.create(name: I18n.t('spree.wishlist.default_wishlist_name'), is_default: true)
     default_wishlist.update_attribute(:is_default, true) unless default_wishlist.is_default?
     default_wishlist
   end
